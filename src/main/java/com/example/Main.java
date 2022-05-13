725e830873cbd15743fa81cad2ce6f418733d15d
thanhphat219 <phat01283249260@gmail.com>
2022-05-13 15:48:13 +0700

    9

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,8 +5,12 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,6 +23,8 @@ import java.util.ArrayList;
 import java.util.Map;
 
 @Controller
+@Configuration
+@EnableJpaRepositories
 @SpringBootApplication
 public class Main {
 //
