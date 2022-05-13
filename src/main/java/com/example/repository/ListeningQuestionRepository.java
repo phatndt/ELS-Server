e94fdd3889287d3e6e2728828d5c95d09f55bfdc
thanhphat219 <phat01283249260@gmail.com>
2022-05-13 16:49:34 +0700

    150

--- src/main/java/com/example/repository/ListeningQuestionRepository.java
+++ src/main/java/com/example/repository/ListeningQuestionRepository.java
@@ -3,6 +3,8 @@ package com.example.repository;
 import com.example.model.Listening;
 import com.example.model.ListeningQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ListeningQuestionRepository extends JpaRepository<ListeningQuestion, String> {
 }
