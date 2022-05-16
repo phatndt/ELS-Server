0ff1a49bb32dad44e608133b61000da141a59ae7
thanhphat219 <phat01283249260@gmail.com>
2022-05-16 10:09:03 +0700

    25

--- src/main/java/com/example/controller/ListeningQuestionController.java
+++ src/main/java/com/example/controller/ListeningQuestionController.java
@@ -1,9 +1,11 @@
 package com.example.controller;
 
+import com.example.exception.ResourceNotFoundException;
 import com.example.model.ListeningQuestion;
 import com.example.repository.ListeningQuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,11 @@ public class ListeningQuestionController {
     @GetMapping("/listeningQuestion")
     public List<ListeningQuestion> getAllListeningQuestion() {
         return listeningQuestionRepository.findAll();
+    }
+
+    @GetMapping("/listeningQuestion/{id}")
+    @SuppressWarnings (value="unchecked")
+    public List<ListeningQuestion> getListeningQuestionByListeningLesson(@PathVariable String id) {
+        return (List<ListeningQuestion>) listeningQuestionRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource not found"));
     }
 }
